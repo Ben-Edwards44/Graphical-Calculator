2c460a8a51c485651b3207270a3d359973037df4
Ben-Edwards44 <benjedwards007@icloud.com>
2024-12-22 19:26:04 +0000

    Started on square matrices

--- matrix_utils.py
+++ matrix_utils.py
@@ -27,4 +27,47 @@ class Matrix:
     
 
 class SquareMatrix(Matrix):
-    pass
+    def __init__(self, items):
+        super().__init__(items)
+
+    def get_minor(self, remove_x, remove_y):
+        minor_items = []
+        for row_index, row in enumerate(self.items):
+            if row_index == remove_x: continue
+
+            minor_row = [item for col_index, item in enumerate(row) if col_index != remove_y]
+            minor_items.append(minor_row)
+
+        minor = SquareMatrix(minor_items)
+
+        return minor
+
+    def determinant(self):
+        #return the determinant of the matrix
+        if self.width == 2 and self.height == 2:
+            #det = a*d - b*c
+            return self.items[0][0] * self.items[1][1] - self.items[0][1] * self.items[1][0]
+        
+        det = 0
+        for remove_y in range(self.width):
+            #recursively get the determinants of each matrix minor from the top row
+            minor = self.get_minor(0, remove_y)
+            minor_det = minor.determinant()
+
+            if remove_y % 2 == 0:
+                multiplier = self.items[0][remove_y]
+            else:
+                multiplier = -self.items[0][remove_y]
+
+            det += multiplier * minor_det
+
+        return det
+    
+
+a = SquareMatrix([[5, 8, 9, 2],
+                  [1, 5, 8, 6],
+                  [2, 4, 3, 3],
+                  [9, 0, 1, 2]])
+
+
+print(a.determinant())
