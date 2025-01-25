7a68104d64c2c764bbdff86ba7cda0034c94d811
Ben-Edwards44 <benjedwards007@icloud.com>
2025-01-25 21:38:43 +0000

    Updated about mode

--- about.py
+++ about.py
@@ -3,23 +3,44 @@ import pygame
 
 
 ABOUT_TEXT = """
-This is a calculator.
-I will write the rest later...
+This is a graphical calculator emulator.
+It is intended to help with revision for A level
+maths and further maths, but can be
+useful for any level of maths.
+
+The calculator has four main modes:
+- Calculator: Performs simple calculations
+and evaluates expressions
+- 2D grapher: Draws graphs of 2D funcitons
+- Simul equation: Solves systems of linear
+simultaneous equations
+- Equation: Finds approximate solutions to a
+single equation
 """
 
+FONT_SIZE = 30
 
-TEXT_Y = 50
+TEXT_Y = 60
 
-LINE_PADDING_Y = 50
+LINE_PADDING_Y = 36
 
 
-def setup_text():
+def setup_heading_text():
+    heading_text = gui.DisplayText("About", gui.HEADING_CENTER_POS)
+    heading_text.set_font_size(gui.HEADING_FONT_SIZE)
+
+    return heading_text
+
+
+def setup_about_text():
     #pygame cannot render newlines, so a new text object needs to be created for each line
     lines = ABOUT_TEXT.splitlines()
 
     text_objects = []
     for inx, line in enumerate(lines):
         text_object = gui.DisplayText(line, (0, 0))
+        text_object.set_font_size(FONT_SIZE)
+        
         width = text_object.get_width()
 
         y = TEXT_Y + inx * LINE_PADDING_Y
@@ -44,7 +65,11 @@ def draw(window, text_objects, back_button):
 
 
 def main(window):
-    text_objects = setup_text()
+    text_objects = setup_about_text()
+    heading_text = setup_heading_text()
+
+    text_objects.append(heading_text)
+
     back_button = gui.create_back_button()
 
     while not back_button.is_clicked():
